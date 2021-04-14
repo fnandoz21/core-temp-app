24b75bdfe4e02f655aad3bf091bf9d5365efa86b
Fernando Zapata <fzlakers1029@gmail.com>
2021-04-14 14:42:17 -0400

    attempt to get axis labels for 5 points

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
 import 'dart:typed_data';
+import 'package:intl/intl.dart';
 
 import 'package:flutter/material.dart';
 // import 'package:flutter_blue/flutter_blue.dart';
@@ -52,20 +53,21 @@ class _MyHomePageState extends State<MyHomePage> {
   List<BluetoothDevice> devicesList = [];
   int weirdLeadingDigit = 0;
   int maxPlotPoints = 8;
-  Future<void> getpaireddevices() async{
+  Future<void> getpaireddevices() async {
     List<BluetoothDevice> devices = [];
-    try{
+    try {
       devices = await FlutterBluetoothSerial.instance.getBondedDevices();
-    } on PlatformException{
+    } on PlatformException {
       print("platform exception");
     }
-    setState((){
+    setState(() {
       devicesList = devices;
-      for(int i=0;i<devices.length;i++){
+      for (int i = 0; i < devices.length; i++) {
         print(devices[i].address);
       }
     });
   }
+
   _addDeviceTolist(final BluetoothDevice device) {
     if (!widget.devicesList.contains(device)) {
       setState(() {
@@ -77,7 +79,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-
 
     // Get current state
     FlutterBluetoothSerial.instance.state.then((state) {
@@ -97,42 +98,45 @@ class _MyHomePageState extends State<MyHomePage> {
     }).then((_) async {
       // Some simplest connection :F
       try {
-        BluetoothConnection connection = await BluetoothConnection.toAddress(
-            _address);
+        BluetoothConnection connection =
+            await BluetoothConnection.toAddress(_address);
         print('Connected to the device');
 
         connection.input.listen((Uint8List data) {
           //print('Data incoming: ${ascii.decode(data)}');
-          if (data.length==1){
-            try{
+          if (data.length == 1) {
+            try {
               weirdLeadingDigit = int.parse(ascii.decode(data));
-            } on FormatException{
+            } on FormatException {
               print('Expected Numerical string, got ${ascii.decode(data)}');
             }
-          }
-          else if(data.length==5) {
-            double thisPoint = double.parse(ascii.decode(data))+10*weirdLeadingDigit;
+          } else if (data.length == 5) {
+            double thisPoint =
+                double.parse(ascii.decode(data)) + 10 * weirdLeadingDigit;
             var now = DateTime.now();
+            String nowTime = DateFormat.Hms().format(now);
+            print(nowTime);
             print(thisPoint);
-            setState((){
-              temps.add([thisPoint,now]);
+            setState(() {
+              temps.add([thisPoint, nowTime]);
               if (temps.length > 1800) {
                 temps.removeFirst();
               }
             });
-          }
-          else{
+          } else {
             double thisPoint = double.parse(ascii.decode(data));
             var now = DateTime.now();
+            String nowTime = DateFormat.Hms().format(now);
+            print(nowTime);
             print(thisPoint);
-            setState((){
-              temps.add([thisPoint,now]);
+            setState(() {
+              temps.add([thisPoint, nowTime]);
               if (temps.length > 1800) {
                 temps.removeFirst();
               }
             });
           }
-         // print(ascii.decode(data));
+          // print(ascii.decode(data));
           connection.output.add(data); // Sending data
 
           if (ascii.decode(data).contains('!')) {
@@ -142,8 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
         }).onDone(() {
           print('Disconnected by remote request');
         });
-      }
-      catch (exception) {
+      } catch (exception) {
         print('Cannot connect, exception occurred');
       }
       // Update the address field
@@ -174,187 +177,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // ListView _buildListViewOfDevices() {
-  //   List<Container> containers = new List<Container>();
-  //   for (BluetoothDevice device in widget.devicesList) {
-  //     containers.add(
-  //       Container(
-  //         height: 50,
-  //         child: Row(
-  //           children: <Widget>[
-  //             Expanded(
-  //               child: Column(
-  //                 children: <Widget>[
-  //                   Text(device.name == '' ? '(unknown device)' : device.name),
-  //                   Text(device.id.toString()),
-  //                 ],
-  //               ),
-  //             ),
-  //             FlatButton(
-  //               color: Colors.blue,
-  //               child: Text(
-  //                 'Connect',
-  //                 style: TextStyle(color: Colors.white),
-  //               ),
-  //               onPressed: () async {
-  //                 widget.flutterBlue.stopScan();
-  //                 try {
-  //                   await device.connect();
-  //                 } catch (e) {
-  //                   if (e.code != 'already_connected') {
-  //                     throw e;
-  //                   }
-  //                 } finally {
-  //                   _services = await device.discoverServices();
-  //                 }
-  //                 setState(() {
-  //                   _connectedDevice = device;
-  //                 });
-  //               },
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //     );
-  //   }
-
-  //   return ListView(
-  //     padding: const EdgeInsets.all(8),
-  //     children: <Widget>[
-  //       ...containers,
-  //       Text('123',
-  //       textAlign: TextAlign.center,
-  //       style: TextStyle(
-  //       fontSize: 80,
-  //       color: Colors.black,
-  //       // foreground: Paint()
-  //       //   ..style = PaintingStyle.stroke
-  //       //   ..strokeWidth = 6
-  //       //   ..color = Colors.blue,
-  //     ),
-  //       ),
-  //       TextButton.icon(
-  //         label: Text('START TEMP CHECK'),
-  //         style: TextButton.styleFrom(
-  //           primary: Colors.white,
-  //           backgroundColor: Colors.blue,
-  //           onSurface: Colors.grey,
-  //           textStyle: TextStyle(fontSize: 20),
-  //         ),
-  //         icon: Icon(Icons.device_thermostat
-  //         ),
-  //         onPressed: () {
-  //           print('no cap');
-  //   }
-  // )
-  //     ],
-  //   );
-  // }
-
-  // List<ButtonTheme> _buildReadWriteNotifyButton(
-  //     BluetoothCharacteristic characteristic) {
-  //   List<ButtonTheme> buttons = new List<ButtonTheme>();
-
-  //   if (characteristic.properties.read) {
-  //     buttons.add(
-  //       ButtonTheme(
-  //         minWidth: 10,
-  //         height: 20,
-  //         child: Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 4),
-  //           child: RaisedButton(
-  //             color: Colors.blue,
-  //             child: Text('READ', style: TextStyle(color: Colors.white)),
-  //             onPressed: () async {
-  //               var sub = characteristic.value.listen((value) {
-  //                 setState(() {
-  //                   widget.readValues[characteristic.uuid] = value;
-  //                 });
-  //               });
-  //               await characteristic.read();
-  //               sub.cancel();
-  //             },
-  //           ),
-  //         ),
-  //       ),
-  //     );
-  //   }
-  //   if (characteristic.properties.write) {
-  //     buttons.add(
-  //       ButtonTheme(
-  //         minWidth: 10,
-  //         height: 20,
-  //         child: Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 4),
-  //           child: RaisedButton(
-  //             child: Text('WRITE', style: TextStyle(color: Colors.white)),
-  //             onPressed: () async {
-  //               await showDialog(
-  //                   context: context,
-  //                   builder: (BuildContext context) {
-  //                     return AlertDialog(
-  //                       title: Text("Write"),
-  //                       content: Row(
-  //                         children: <Widget>[
-  //                           Expanded(
-  //                             child: TextField(
-  //                               controller: _writeController,
-  //                             ),
-  //                           ),
-  //                         ],
-  //                       ),
-  //                       actions: <Widget>[
-  //                         FlatButton(
-  //                           child: Text("Send"),
-  //                           onPressed: () {
-  //                             characteristic.write(
-  //                                 utf8.encode(_writeController.value.text));
-  //                             Navigator.pop(context);
-  //                           },
-  //                         ),
-  //                         FlatButton(
-  //                           child: Text("Cancel"),
-  //                           onPressed: () {
-  //                             Navigator.pop(context);
-  //                           },
-  //                         ),
-  //                       ],
-  //                     );
-  //                   });
-  //             },
-  //           ),
-  //         ),
-  //       ),
-  //     );
-  //   }
-  //   if (characteristic.properties.notify) {
-  //     buttons.add(
-  //       ButtonTheme(
-  //         minWidth: 10,
-  //         height: 20,
-  //         child: Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 4),
-  //           child: RaisedButton(
-  //             child: Text('NOTIFY', style: TextStyle(color: Colors.white)),
-  //             onPressed: () async {
-  //               characteristic.value.listen((value) {
-  //                 widget.readValues[characteristic.uuid] = value;
-  //               });
-  //               await characteristic.setNotifyValue(true);
-  //             },
-  //           ),
-  //         ),
-  //       ),
-  //     );
-  //   }
-
-  //   return buttons;
-  // }
-
-  // void setCharacteristic(){
-
-  // }
-
   void appendTemp(tempChar) async {
     await tempChar.setNotifyValue(true);
     tempChar.value.listen((value) {
@@ -365,8 +187,7 @@ class _MyHomePageState extends State<MyHomePage> {
           temps.removeFirst();
         }
       });
-    }
-    );
+    });
   }
 
   ListView _buildConnectDeviceView() {
@@ -375,33 +196,36 @@ class _MyHomePageState extends State<MyHomePage> {
     // for (BluetoothService service in _services) {
     //   List<Widget> characteristicsWidget = new List<Widget>();
     //   for (BluetoothCharacteristic characteristic in service.characteristics) {
-    containers.add(
-        Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(temps.last.toString() + '°C' ,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textScaleFactor: 5.0,
-            )
-        )
-    );
+    containers.add(Container(
+        padding: EdgeInsets.all(10.0),
+        child: Text(
+          temps.last.toString() + '°C',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.bold),
+          textScaleFactor: 5.0,
+        )));
     List<FlSpot> tempData = List.generate(temps.length, (index) {
       return FlSpot(index.toDouble(), temps.elementAt(index).elementAt(0));
     });
-    containers.add(
-        Container(
-            padding: EdgeInsets.all(10),
-            width: double.infinity,
-            child: LineChart(LineChartData(
-                borderData: FlBorderData(show: false),
-                lineBarsData: [
-                  LineChartBarData(
-                      spots: tempData
-                  )
-                ]
-            ))
-        )
-    );
+    containers.add(Container(
+        padding: EdgeInsets.all(10),
+        width: double.infinity,
+        child: LineChart(LineChartData(
+            borderData: FlBorderData(show: false),
+            lineBarsData: [LineChartBarData(spots: tempData)],
+            titlesData: FlTitlesData(
+              bottomTitles: SideTitles(
+                showTitles: true,
+                getTitles: (val) {
+                  if (val.toInt() % (temps.length~/5) == 0) {
+                      return temps.elementAt(val.toInt()).elementAt(1);
+                  }
+                  else {
+                    return '';
+                  }
+                }
+              ))),
+            )));
     //     characteristicsWidget.add(
     //       Align(
     //         alignment: Alignment.centerLeft,
@@ -457,8 +281,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(
+  Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Text("Core Temp"),
         ),
