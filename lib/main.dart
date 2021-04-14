c9f28d5cf1f2d5ac9f705eec3d658333b6ef5ba7
Will512 <wrs18@duke.edu>
2021-04-13 22:16:58 -0400

    Code fully functional with classic bluetooth

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 // import 'package:flutter_blue/flutter_blue.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() => runApp(MyApp());
@@ -36,7 +37,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
 
-  String _address = "002004BD185B";
+  String _address = "00:20:04:BD:18:5B";
   String _name = "CORE TEMP SENSOR";
 
   // Timer _discoverableTimeoutTimer;
@@ -44,10 +45,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final _writeController = TextEditingController();
   BluetoothDevice _connectedDevice;
-  // List<BluetoothService> _services;
-  Queue temps = new Queue.from([1.0,2.0,3.0,4.0,5.0]);
 
-  
+  // List<BluetoothService> _services;
+  //Queue temps = new Queue.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+  Queue temps = new Queue();
+  List<BluetoothDevice> devicesList = [];
+  int weirdLeadingDigit = 0;
+  int maxPlotPoints = 8;
+  Future<void> getpaireddevices() async{
+    List<BluetoothDevice> devices = [];
+    try{
+      devices = await FlutterBluetoothSerial.instance.getBondedDevices();
+    } on PlatformException{
+      print("platform exception");
+    }
+    setState((){
+      devicesList = devices;
+      for(int i=0;i<devices.length;i++){
+        print(devices[i].address);
+      }
+    });
+  }
   _addDeviceTolist(final BluetoothDevice device) {
     if (!widget.devicesList.contains(device)) {
       setState(() {
@@ -60,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
 
-    
+
     // Get current state
     FlutterBluetoothSerial.instance.state.then((state) {
       setState(() {
@@ -68,6 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     });
 
+    getpaireddevices();
     Future.doWhile(() async {
       // Wait if adapter not enabled
       if (await FlutterBluetoothSerial.instance.isEnabled) {
@@ -78,23 +97,54 @@ class _MyHomePageState extends State<MyHomePage> {
     }).then((_) async {
       // Some simplest connection :F
       try {
-          BluetoothConnection connection = await BluetoothConnection.toAddress(_address);
-          print('Connected to the device');
+        BluetoothConnection connection = await BluetoothConnection.toAddress(
+            _address);
+        print('Connected to the device');
 
-          connection.input.listen((Uint8List data) {
-              print('Data incoming: ${ascii.decode(data)}');
-              connection.output.add(data); // Sending data
-
-              if (ascii.decode(data).contains('!')) {
-                  connection.finish(); // Closing connection
-                  print('Disconnecting by local host');
+        connection.input.listen((Uint8List data) {
+          //print('Data incoming: ${ascii.decode(data)}');
+          if (data.length==1){
+            try{
+              weirdLeadingDigit = int.parse(ascii.decode(data));
+            } on FormatException{
+              print('Expected Numerical string, got ${ascii.decode(data)}');
+            }
+          }
+          else if(data.length==5) {
+            double thisPoint = double.parse(ascii.decode(data))+10*weirdLeadingDigit;
+            var now = DateTime.now();
+            print(thisPoint);
+            setState((){
+              temps.add([thisPoint,now]);
+              if (temps.length > 1800) {
+                temps.removeFirst();
               }
-          }).onDone(() {
-              print('Disconnected by remote request');
-          });
+            });
+          }
+          else{
+            double thisPoint = double.parse(ascii.decode(data));
+            var now = DateTime.now();
+            print(thisPoint);
+            setState((){
+              temps.add([thisPoint,now]);
+              if (temps.length > 1800) {
+                temps.removeFirst();
+              }
+            });
+          }
+         // print(ascii.decode(data));
+          connection.output.add(data); // Sending data
+
+          if (ascii.decode(data).contains('!')) {
+            connection.finish(); // Closing connection
+            print('Disconnecting by local host');
+          }
+        }).onDone(() {
+          print('Disconnected by remote request');
+        });
       }
       catch (exception) {
-          print('Cannot connect, exception occured');
+        print('Cannot connect, exception occurred');
       }
       // Update the address field
       // FlutterBluetoothSerial.instance.address.then((address) {
@@ -305,112 +355,113 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // }
 
-  void appendTemp(tempChar) async{
+  void appendTemp(tempChar) async {
     await tempChar.setNotifyValue(true);
     tempChar.value.listen((value) {
-      setState((){
-      var temp = value[0].toDouble() / 100;
-      temps.add(temp);
-      if (temps.length > 1800) {
-        temps.removeFirst();
-      }
-    });
-          }
+      setState(() {
+        var temp = value[0].toDouble() / 100;
+        temps.add(temp);
+        if (temps.length > 1800) {
+          temps.removeFirst();
+        }
+      });
+    }
     );
   }
+
   ListView _buildConnectDeviceView() {
     List<Container> containers = new List<Container>();
 
     // for (BluetoothService service in _services) {
     //   List<Widget> characteristicsWidget = new List<Widget>();
     //   for (BluetoothCharacteristic characteristic in service.characteristics) {
-        containers.add(
-            Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text(temps.last.toString() + '°',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  textScaleFactor: 5.0,
-                )
+    containers.add(
+        Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text(temps.last.toString() + '°C' ,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+              textScaleFactor: 5.0,
             )
-        );
-        List<FlSpot> tempData = List.generate(temps.length, (index) {
-          return FlSpot(index.toDouble(), temps.elementAt(index));
-        });
-        containers.add(
-            Container(
-                padding: EdgeInsets.all(10),
-                width: double.infinity,
-                child: LineChart(LineChartData(
-                    borderData: FlBorderData(show: false),
-                    lineBarsData: [
-                      LineChartBarData(
-                          spots: tempData
-                      )
-                    ]
-                ))
-            )
-        );
-        //     characteristicsWidget.add(
-        //       Align(
-        //         alignment: Alignment.centerLeft,
-        //         child: Column(
-        //           children: <Widget>[
-        //             Row(
-        //               children: <Widget>[
-        //                 Text(characteristic.uuid.toString(),
-        //                     style: TextStyle(fontWeight: FontWeight.bold)),
-        //               ],
-        //             ),
-        //             Row(
-        //               children: <Widget>[
-        //                 ..._buildReadWriteNotifyButton(characteristic),
-        //               ],
-        //             ),
-        //             Row(
-        //               children: <Widget>[
-        //                 Text('Value: ' +
-        //                     widget.readValues[characteristic.uuid].toString()),
-        //               ],
-        //             ),
-        //             Divider(),
-        //           ],
-        //         ),
-        //       ),
-        //     );
-        //   }
-        //   containers.add(
-        //     Container(
-        //       child: ExpansionTile(
-        //           title: Text(service.uuid.toString()),
-        //           children: characteristicsWidget),
-        //     ),
-        //   );
-        // }
-
-        return ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            ...containers,
-          ],
-        );
-      // }
+        )
+    );
+    List<FlSpot> tempData = List.generate(temps.length, (index) {
+      return FlSpot(index.toDouble(), temps.elementAt(index).elementAt(0));
+    });
+    containers.add(
+        Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            child: LineChart(LineChartData(
+                borderData: FlBorderData(show: false),
+                lineBarsData: [
+                  LineChartBarData(
+                      spots: tempData
+                  )
+                ]
+            ))
+        )
+    );
+    //     characteristicsWidget.add(
+    //       Align(
+    //         alignment: Alignment.centerLeft,
+    //         child: Column(
+    //           children: <Widget>[
+    //             Row(
+    //               children: <Widget>[
+    //                 Text(characteristic.uuid.toString(),
+    //                     style: TextStyle(fontWeight: FontWeight.bold)),
+    //               ],
+    //             ),
+    //             Row(
+    //               children: <Widget>[
+    //                 ..._buildReadWriteNotifyButton(characteristic),
+    //               ],
+    //             ),
+    //             Row(
+    //               children: <Widget>[
+    //                 Text('Value: ' +
+    //                     widget.readValues[characteristic.uuid].toString()),
+    //               ],
+    //             ),
+    //             Divider(),
+    //           ],
+    //         ),
+    //       ),
+    //     );
+    //   }
+    //   containers.add(
+    //     Container(
+    //       child: ExpansionTile(
+    //           title: Text(service.uuid.toString()),
+    //           children: characteristicsWidget),
+    //     ),
+    //   );
     // }
-  }
-      // NOTE: the if statement and secondary return are only to test UI as if a device were connected
-      ListView _buildView() {
-        //if (_connectedDevice != null) {
-          return _buildConnectDeviceView();
-      //  }
-       // return _buildListViewOfDevices();
-      }
 
-      @override
-      Widget build(BuildContext context) =>
-          Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title),
-            ),
-            body: _buildView(),
-          );
-    }
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  // }
+  // NOTE: the if statement and secondary return are only to test UI as if a device were connected
+  ListView _buildView() {
+    //if (_connectedDevice != null) {
+    return _buildConnectDeviceView();
+    //  }
+    // return _buildListViewOfDevices();
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(
+          title: Text("Core Temp"),
+        ),
+        body: _buildView(),
+      );
+}
